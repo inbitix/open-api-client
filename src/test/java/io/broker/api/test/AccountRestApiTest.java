fb9b7b947fa545035e7224f10111bd2471c2bfa7
luke <bing58338@gmail.com>
2026-04-09 15:00:56 +0800

    test commit

--- src/test/java/io/broker/api/test/AccountRestApiTest.java
+++ src/test/java/io/broker/api/test/AccountRestApiTest.java
@@ -39,44 +39,44 @@ public class AccountRestApiTest {
     public void testSpot() {
         BrokerApiRestClient client = getSpotRestClient();
 
-//        System.out.println("\n ------limit buy-----");
-//        NewOrderResponse response1 = client.newOrder(NewOrder.limitBuy("BTCUSDT", TimeInForce.GTC, "0.01", "66000"));
-//        System.out.println(response1);
+        System.out.println("\n ------limit buy-----");
+        NewOrderResponse response1 = client.newOrder(NewOrder.limitBuy("BTCUSDT", TimeInForce.GTC, "0.01", "66000"));
+        System.out.println(response1);
 
-//        System.out.println("\n ------limit sell-----");
-//        NewOrderResponse response2 = client.newOrder(NewOrder.limitSell("BTCUSDT", TimeInForce.GTC, "0.01", "69550"));
-//        System.out.println(response2);
-//
-//        System.out.println("\n ------market buy-----");
-//        NewOrderResponse response3 = client.newOrder(NewOrder.marketBuy("BTCUSDT", "68550"));
-//        System.out.println(response3);
-//
-//        System.out.println("\n ------market sell-----");
-//        NewOrderResponse response4 = client.newOrder(NewOrder.marketSell("BTCUSDT", "0.01"));
-//        System.out.println(response4);
-//
-//        System.out.println("\n ------get order status-----");
-//        Order order = client.getOrderStatus(new OrderStatusRequest(response1.getOrderId()));
-//        System.out.println(order);
-//
-//        System.out.println("\n ------cancel order-----");
-//        CancelOrderResponse cancelOrderResponse = client.cancelOrder(new CancelOrderRequest(response1.getOrderId()));
-//        System.out.println(cancelOrderResponse);
-//
-//        System.out.println("\n ------get open orders-----");
-//        List<Order> openOrderList = client.getOpenOrders(new OpenOrderRequest("BTCUSDT", 5));
-//        System.out.println(openOrderList);
-//
-//        System.out.println("\n ------get history orders-----");
-//        List<Order> historyOrderList = client.getHistoryOrders(new HistoryOrderRequest());
-//        System.out.println(historyOrderList);
-//
-//        System.out.println("\n ------get account information-----");
-//        Account account = client.getAccount(BrokerConstants.DEFAULT_RECEIVING_WINDOW, System.currentTimeMillis());
-//        System.out.println(account);
-//        System.out.println(account.getBalances());
-//        System.out.println(account.getAssetBalance("BTC"));
-//
+        System.out.println("\n ------limit sell-----");
+        NewOrderResponse response2 = client.newOrder(NewOrder.limitSell("BTCUSDT", TimeInForce.GTC, "0.01", "69550"));
+        System.out.println(response2);
+
+        System.out.println("\n ------market buy-----");
+        NewOrderResponse response3 = client.newOrder(NewOrder.marketBuy("BTCUSDT", "68550"));
+        System.out.println(response3);
+
+        System.out.println("\n ------market sell-----");
+        NewOrderResponse response4 = client.newOrder(NewOrder.marketSell("BTCUSDT", "0.01"));
+        System.out.println(response4);
+
+        System.out.println("\n ------get order status-----");
+        Order order = client.getOrderStatus(new OrderStatusRequest(response1.getOrderId()));
+        System.out.println(order);
+
+        System.out.println("\n ------cancel order-----");
+        CancelOrderResponse cancelOrderResponse = client.cancelOrder(new CancelOrderRequest(response1.getOrderId()));
+        System.out.println(cancelOrderResponse);
+
+        System.out.println("\n ------get open orders-----");
+        List<Order> openOrderList = client.getOpenOrders(new OpenOrderRequest("BTCUSDT", 5));
+        System.out.println(openOrderList);
+
+        System.out.println("\n ------get history orders-----");
+        List<Order> historyOrderList = client.getHistoryOrders(new HistoryOrderRequest());
+        System.out.println(historyOrderList);
+
+        System.out.println("\n ------get account information-----");
+        Account account = client.getAccount(BrokerConstants.DEFAULT_RECEIVING_WINDOW, System.currentTimeMillis());
+        System.out.println(account);
+        System.out.println(account.getBalances());
+        System.out.println(account.getAssetBalance("BTC"));
+
         System.out.println("\n ------get trades -----");
         List<Trade> tradeList = client.getMyTrades(new MyTradeRequest(5));
         System.out.println(tradeList);
